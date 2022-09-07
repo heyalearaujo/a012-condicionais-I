48a509c05787c28498be9a300d709f8f08e79655
heyalearaujo <alexandrearza@gmail.com>
2022-09-06 21:37:40 -0300

    Resolução exercicio 1 - Condicionais1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,6 @@
-const num = //seu numero aqui
-
-if(){
-    console.log("sua mensagem aqui");
+const num = Number(prompt("Insira um número"))
+if(num % 2 === 0){
+    console.log("Este númeor é par!");
+}else if(num % 2 === 1) {
+    console.log("Este númeor é impar!");
 }
