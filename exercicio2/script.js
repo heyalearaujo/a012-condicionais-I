2809c290a69b2f9d7f5086f69b0c3f826760d9f8
heyalearaujo <alexandrearza@gmail.com>
2022-09-06 21:38:12 -0300

    Resolução exercicio 2 - Condicionais1

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt("Qual sua idade?")
+let ensinoMedio = confirm("Terminou o ensino médio?")
+let faculdade = confirm("Você faz faculdade?")
 
-if(){
+if(idade>=18){
+    console.log("Você é MAIOR de idade!")
 
 }else{
-	
+	console.log("Você é MENOR de idade!")
+}
+//(ensinoMedio)
+if(ensinoMedio === true){
+    console.log("Você terminou o Ensino Médio!")
+}else{
+    console.log("Você NÂO terminou o Ensino Médio!")
+}
+//(!faculdade)
+if(faculdade === true){
+    console.log("Você está na Faculdade!")
+}else{
+    console.log("Você NÂO está na Faculdade!")
 }
