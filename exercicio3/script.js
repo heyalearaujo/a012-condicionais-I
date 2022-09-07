e29d9622b41ff1a2f9ce36ca3b2878b88cc1980b
heyalearaujo <alexandrearza@gmail.com>
2022-09-06 21:38:40 -0300

    Resolução exercicio 3 - Condicionais1

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,20 @@
-let nacionalidade //= adicione o prompt aqui
+const nacionalidade = Number(prompt("Qual a sua nacionalidade?"))
+const b = ("brasileiro")
+const a = ("argentino")
+const u = ("uruguaio")
+const c = ("chileno")
+const cc = ("colombiano")
+
+if(nacionalidade === b){
+    console.log("Você é Brasileiro!")
+} else if(nacionalidade === a){
+    console.log("Você é Argentino!")
+}else if(nacionalidade === u){
+    console.log("Você é Uruguaio!")
+}else if(nacionalidade === c){
+    console.log("Você é Chileno!")
+}else if(nacionalidade === cc){
+    console.log("Você é Colombiano!")
+}else{
+    console.log("Nascionalidade não encontrada!!")
+}
